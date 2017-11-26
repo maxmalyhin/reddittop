92518b22e0d585f558c21ca656d75a5329dbc55d
Maksym Malyhin <mmalyhin@lixar.com>
2017-11-26 11:53:28 -0400

    Initial project structure created

---
+++ RedditClient/Source/RedditClientCore/RedditClientCore/RedditClientCore.h
@@ -0,0 +1,19 @@
+//
+//  RedditClientCore.h
+//  RedditClientCore
+//
+//  Created by Maksym Malyhin on 2017-11-26.
+//  Copyright © 2017 Maksym Malyhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RedditClientCore.
+FOUNDATION_EXPORT double RedditClientCoreVersionNumber;
+
+//! Project version string for RedditClientCore.
+FOUNDATION_EXPORT const unsigned char RedditClientCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RedditClientCore/PublicHeader.h>
+
+
